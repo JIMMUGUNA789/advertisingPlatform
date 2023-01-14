3309267e41966487b68f0463860e6d80d8f621b2
JIMMUGUNA789 <jimmuguna@gmail.com>
2023-01-14 13:30:35 +0000

    message alerts added

--- AdvertApp/settings.py
+++ AdvertApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,10 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000'
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
